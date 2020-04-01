b4aaa89dd09a41e8eac04b0333b326b188823f46
rgiovani <ronaldo-giovani07@hotmail.com>
2020-04-01 20:14:31 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+const routes = require('./routes');
+
+app.set('view engine', 'ejs');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded()); //middleware
+app.use(express.static(__dirname + '/public')); //importa os arquivos static (styles e css)
+
+app.use(routes) 
+
+app.listen(port, () => {
+  console.log(`listening in port ${port}`);
+});
