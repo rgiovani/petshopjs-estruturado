e5f7afef665e17e1cb2e8602ec84574dd6f190e0
rgiovani <ronaldo-giovani07@hotmail.com>
2020-04-16 10:24:12 -0300

    layout ajustado e novos forms.

--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,14 @@ const Fornecedor = require('./models/Fornecedor');
 const Telefone = require('./models/Telefone');
 
 routes.get('/', async (req, res)=>{
+    res.render('pages/home');
+})
+
+routes.get('/cliente', async (req, res)=>{
     {/* isso é uma instancia do objeto e ao mesmo tempo persistindo no banco de dados */}
     const clientes = await Cliente.findAll();
     
-    res.render('pages/home', {
+    res.render('pages/cliente', {
         clientes:  clientes
     });
 })
@@ -23,14 +27,24 @@ routes.get('/fornecedor', async (req, res)=>{
     });
 })
 
-routes.get('/form', (req, res)=>{
-    res.render('pages/form');
+routes.get('/clienteForm', (req, res)=>{
+    res.render('pages/clienteForm');
+})
+
+routes.get('/fornecedorForm', (req, res)=>{
+    res.render('pages/fornecedorForm');
 })
 
 routes.post('/Cliente/add', async(req,res)=>{
     let {nome, sobrenome, email, cpf} = req.body;
     const clientes = await Cliente.create({nome, sobrenome, email, cpf});
-    res.redirect('/');
+    res.redirect('/cliente');
+})
+
+routes.post('/Fornecedor/add', async(req,res)=>{
+    let {nome, sobrenome, email, cnpj} = req.body;
+    const fornecedores = await Fornecedor.create({nome, sobrenome, email, cnpj});
+    res.redirect('/fornecedor');
 })
 
 module.exports = routes;
